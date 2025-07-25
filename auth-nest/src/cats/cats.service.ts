7b3bafdec22f12b03228a82c2c3bcf23e281f580
lovepreet75way <lovepreet@75way.com>
2025-07-25 18:49:43 +0530

    lerned all http request , controller, injector using nest.js

--- auth-nest/src/cats/cats.service.ts
+++ auth-nest/src/cats/cats.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 
 export interface Cat {
+  id: number;
   name: string;
   age: number;
 }
@@ -8,13 +9,40 @@ export interface Cat {
 @Injectable()
 export class CatsService {
   private cats: Cat[] = [];
+  private nextId = 1;
 
   findAll(): Cat[] {
     return this.cats;
   }
 
-  create(cat: Cat): Cat {
-    this.cats.push(cat);
+  findOne(id: number): Cat | undefined {
+    return this.cats.find(cat => cat.id === id);
+  }
+
+  create(cat: Omit<Cat, 'id'>): Cat {
+    const newCat = { id: this.nextId++, ...cat };
+    this.cats.push(newCat);
+    return newCat;
+  }
+
+  update(id: number, cat: Cat): Cat | undefined {
+    const index = this.cats.findIndex(c => c.id === id);
+    if (index === -1) return undefined;
+    this.cats[index] = cat;
     return cat;
+  }
+
+  partialUpdate(id: number, catPatch: Partial<Cat>): Cat | undefined {
+    const cat = this.findOne(id);
+    if (!cat) return undefined;
+    Object.assign(cat, catPatch);
+    return cat;
+  }
+
+  remove(id: number): boolean {
+    const index = this.cats.findIndex(cat => cat.id === id);
+    if (index === -1) return false;
+    this.cats.splice(index, 1);
+    return true;
   }
 }
